77528f17f4fe994175967fe4b7e5c5202c9d95b2
= <=>
2025-03-10 21:29:58 +0100

    Removing luasnip

--- lua/plugins/new.lua
+++ lua/plugins/new.lua
@@ -1,16 +1,7 @@
 return {
     {
-        "aserowy/tmux.nvim",
-        config = function()
-            return require("tmux").setup()
-        end,
-    },
-    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-    },
-    {
-        "phelipetls/vim-hugo",
     },
 }
