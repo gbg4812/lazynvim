f4f4f98bf0f8cdf4fb2414ef47962ebaafa68293
Guillem Baldi <guillembaldi@gmail.com>
2026-01-04 19:53:44 +0100

    clean config

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -21,32 +21,6 @@ return {
     },
 
     {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                texlab = {
-                    settings = {
-                        texlab = {
-                            build = {
-                                executable = "latexmk",
-                                args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-                            },
-                            forwardSearch = {
-                                executable = "zathura",
-                                args = { "--synctex-forward", "%l:1:%f", "%p" },
-                            },
-                            formatterLineLength = 80, -- 👈 wrap lines at 80 columns
-                            latexindent = {
-                                modifyLineBreaks = true, -- 👈 actually allows reflowing text
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    },
-
-    {
         "lervag/vimtex",
         lazy = false, -- lazy-loading will disable inverse search
         config = function()
@@ -55,31 +29,6 @@ return {
         end,
         keys = {
             { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
-        },
-    },
-
-    {
-        "folke/noice.nvim",
-        opts = {
-            lsp = {
-                enable = false,
-                message = {
-                    enable = false,
-                },
-            },
-            -- routes = {
-            --     {
-            --         filter = {
-            --             event = "lsp",
-            --             kind = "progress",
-            --             cond = function(message)
-            --                 local client = vim.tbl_get(message.opts, "progress", "client")
-            --                 return client == "ltex_plus"
-            --             end,
-            --         },
-            --         opts = { skip = true },
-            --     },
-            -- },
         },
     },
 
@@ -113,7 +62,6 @@ return {
                                     },
                                 },
                             },
-                            checkFrequency = "save",
                         },
                     },
                 },
